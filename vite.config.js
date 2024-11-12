833fdcbc28c1cbde1b86a8b5ffa4377dd7d32db1
Shahryar-Sohail <shahryar.ch123@gmail.com>
2024-11-12 23:43:16 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.mkv'], // Include .mkv files as static assets
+  base: '/xNeurons/',
 })
